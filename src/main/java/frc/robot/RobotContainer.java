c8677006a92659aa0ffe95e19e726e13eda37dcb
TylerS1066 <TylerS1066@users.noreply.github.com>
2025-05-03 18:17:27 -0500

    Create simple odometry class

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,11 +11,13 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
   private final Drive drive = new Drive();
+  private final Gyro gyro = new Gyro();
+  private final Odometry odometry = new Odometry(gyro::getAngleZ, drive::getLeft, drive::getRight);
   private final DriveCommands driveCommands = new DriveCommands(drive);
+
   private final Arm arm = new Arm();
 
   private final Accelerometer accelerometer = new Accelerometer();
-  private final Gyro gyro = new Gyro();
   private final Rangefinder rangefinder = new Rangefinder();
 
   private final CommandXboxController controller = new CommandXboxController(0);
@@ -27,6 +29,7 @@ public class RobotContainer {
   private void configureButtonBindings() {
     drive.setDefaultCommand(driveCommands.drive(controller::getLeftY, controller::getLeftX));
     controller.leftStick().onTrue(driveCommands.reverse());
+    controller.start().onTrue(odometry.resetAngle());
 
     controller.a()
         .onTrue(ArmCommands.angleCommand(arm, 0))
